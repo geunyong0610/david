afa5e4ec3841ee3c871b032d44f0c455a8f471b9
kgy <petty001@naver.com>
2025-08-04 15:27:12 +0900

    Feat: 컴퓨터 이름 표시 기능 추가 menu.html을 추가하고 /menu 라우트 작성함.
    Resolves: #2
    
    Feat: 컴퓨터 이름 표시 기능 추가
    Flask 앱에 호스트네임 표시 기능 추가 및 디버그 모드 설정.
    
    Resolves: #2

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, Response, render_template
+from flask import Flask, request, Response, render_template 
+import socket
 import os
 from io import BytesIO
 from gtts import gTTS
+
 
 DEFAULT_LANG = os.getenv('DEFAULT_LANG', 'ko')
 app = Flask(__name__)
@@ -19,7 +21,13 @@ def home():
 
 @app.route("/menu")
 def menu():
-    return render_template("menu.html") # 사용자가 /menu로 접속시 Flask가 이 파일 찾아서 내용 읽고 html형태로 브라우저한테 보내줌
+    if app.debug:
+        hostname = '컴퓨터(인스턴스)' + socket.gethostname()
+    else:
+        hostname = ' '
+    
+
+    return render_template("menu.html", computername=hostname) # 사용자가 /menu로 접속시 Flask가 이 파일 찾아서 내용 읽고 html형태로 브라우저한테 보내줌
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 80)
+    app.run('0.0.0.0', 80, debug=True)#Flask가 개발자를 위해 자동 리로드+ 상세오류페이지 제공(Flask자체의 디버깅 기능 
